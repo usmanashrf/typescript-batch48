b13fcebc6d9454d6ebad9711b6cf16d855135918
usmanashrf <45431758+usmanashrf@users.noreply.github.com>
2023-07-08 20:14:20 +0500

    added today's evenig section code

---
+++ Evening-section/class-1/index.js
@@ -0,0 +1,7 @@
+var myName = 'Usman'; // string dataType
+//let, const, var  _
+var age = 25; // number 
+var address = "ISB";
+
+console.log("My name is ", myName);
+console.log("age is ", age);
---
+++ Evening-section/class-1/index.ts
@@ -0,0 +1,10 @@
+let myName = 'Usman'  // string dataType
+
+//let, const, var  _
+
+const age = 25;   // number 
+
+var address ="ISB";
+
+console.log("My name is ", myName);
+console.log("age is ", age);
